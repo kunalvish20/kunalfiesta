59d7156f4cc6ebf37f115c067a85582160ea0877
Kunal Vishwakarma <kunalvishwakarma20@gmail.com>
2025-09-01 21:24:11 +0530

    your commit message

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 export const SUPPORT_CONFIG = {
-    UPI_ID: "Kunalvishwakarma",
-    PAYEE_NAME: "Kunal  Vishwakarma",
+    UPI_ID: "kunalvishwakarma2903@okhdfcbank",
+    PAYEE_NAME: "Kunal Vishwakarma",
     UPI_MSG: "Keep Supporting this Free AI Initiative",
-    BUY_ME_COFFEE_URL: "https://buymeacoffee.com/Kunal ",
+    BUY_ME_COFFEE_URL: "https://buymeacoffee.com/Kunal20 ",
   } as const;
